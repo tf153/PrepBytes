57ce028c9acccfef6bd26cf37d97398d2ca2d2ea
tf153 <43797501+tf153@users.noreply.github.com>
2021-04-10 11:02:15 +0530

    help_clay

---
+++ MarathonApril21/even_fight.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+typedef unsigned long long int ll;
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+}
---
+++ MarathonApril21/help_clay.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int K_small[20000];
+
+long find_nth(int n){
+  int count=0;
+  for(long i=19;;i+=9){
+    int sum=0;
+    for(long x=i;x>0;x/=10)
+      sum+=x%10;
+    if(sum==10)
+      count++;
+    if(count==n)
+      return i;
+  }
+  return -1;
+}
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  int T,n,temp,sum;
+  cin>>T;
+  while(T--){
+    cin>>n;
+    cout<<find_nth(n)<<endl;
+  }
+  return 0;
+}
